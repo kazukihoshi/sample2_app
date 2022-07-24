85ea82a6f8f9642ee27792964c0f4b80586a7ec2
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-07-24 12:21:52 +0000

    [add]6章までfinish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,22 +8,31 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # トップ画面へのリダイレクト
-    redirect_to '/top'
+    # 詳細画面へのリダイレクト
+    redirect_to list_path(list.id)
   end  
 
   def index
+    @lists = List.all
   end
 
   def show
+    # idのレコードを取得
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
   private
   
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
